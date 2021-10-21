ecf29ab985844e5c69ba11a58c0b34bd7f6a5d15
Snake5932 <shtjnk@yandex.ru>
2021-10-21 23:34:30 +0300

    Scripted auto-commit on change (2021-10-21 23:34:30) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/App.java
+++ src/main/java/ru/bmstu/iu9/lab2/App.java
@@ -1,4 +1,15 @@
 package ru.bmstu.iu9.lab2;
 
+import org.apache.hadoop.mapreduce.Job;
+
 public class App {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: App <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(App.class);
+        job.setJobName("App");
+    }
 }
