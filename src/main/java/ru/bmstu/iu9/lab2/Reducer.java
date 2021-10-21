21be7a6e1743900d12213843626ba219ed317825
Snake5932 <shtjnk@yandex.ru>
2021-10-21 20:52:59 +0300

    Scripted auto-commit on change (2021-10-21 20:52:59) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/Reducer.java
+++ src/main/java/ru/bmstu/iu9/lab2/Reducer.java
@@ -1,4 +1,9 @@
 package ru.bmstu.iu9.lab2;
 
-public class Reducer {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class Reducer extends Reducer<Text, IntWritable, Text, LongWritable {
+    
 }
