8178b4dc8d82f6c0c381def39cdda5ddd949b5e8
Snake5932 <shtjnk@yandex.ru>
2021-10-21 21:00:26 +0300

    Scripted auto-commit on change (2021-10-21 21:00:26) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/DelayReducer.java
+++ src/main/java/ru/bmstu/iu9/lab2/DelayReducer.java
@@ -3,12 +3,16 @@ package ru.bmstu.iu9.lab2;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-
+import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
-public class Reducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+public class DelayReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int max = 0, min = 0, avg = 0;
+        int 
+        for (IntWritable val : values) {
 
+        }
     }
 }
