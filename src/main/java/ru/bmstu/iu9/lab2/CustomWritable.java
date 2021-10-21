fafc3f431cbad529eb3b036e5a097287756bf91e
Snake5932 <shtjnk@yandex.ru>
2021-10-21 22:05:41 +0300

    Scripted auto-commit on change (2021-10-21 22:05:41) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
+++ src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
@@ -7,4 +7,21 @@ import org.apache.hadoop.io.Text;
 public class CustomWritable implements Writable {
     private Text airportName;
     private IntWritable delayTime;
+
+    public Text getAirportName() {
+        return airportName;
+    }
+    public void setAirportName(Text airportName) {
+        this.airportName = airportName;
+    }
+
+    public IntWritable getDelayTime() {
+        return delayTime;
+    }
+
+    public void setDelayTime(IntWritable delayTime) {
+        this.delayTime = delayTime;
+    }
+
+    
 }
