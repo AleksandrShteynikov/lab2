1bbb4cf77d45b83b6ed23331f1b4243b3bd9c590
Snake5932 <shtjnk@yandex.ru>
2021-10-21 22:11:24 +0300

    Scripted auto-commit on change (2021-10-21 22:11:24) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
+++ src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
@@ -8,27 +8,32 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class CustomWritable implements Writable {
-    private Text airportName;
-    private IntWritable delayTime;
+    private String airportName;
+    private int delayTime;
 
     public CustomWritable() {}
 
     public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(delayTime);
+        dataOutput.writeUTF(airportName);
+    }
+
+    public void readFields(DataIn dataIn) {
         
     }
 
-    public Text getAirportName() {
+    public String getAirportName() {
         return airportName;
     }
-    public void setAirportName(Text airportName) {
+    public void setAirportName(String airportName) {
         this.airportName = airportName;
     }
 
-    public IntWritable getDelayTime() {
+    public int getDelayTime() {
         return delayTime;
     }
 
-    public void setDelayTime(IntWritable delayTime) {
+    public void setDelayTime(int delayTime) {
         this.delayTime = delayTime;
     }
 
