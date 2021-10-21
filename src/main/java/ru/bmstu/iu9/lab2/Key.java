36c71b5eb48440a4e621979b26d419cba718eadd
Snake5932 <shtjnk@yandex.ru>
2021-10-21 22:30:49 +0300

    Scripted auto-commit on change (2021-10-21 22:30:49) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/Key.java
+++ src/main/java/ru/bmstu/iu9/lab2/Key.java
@@ -1,4 +1,32 @@
 package ru.bmstu.iu9.lab2;
 
-public class Key  {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class Key implements WritableComparable<Key> {
+    private int id;
+    private int dataType;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDataType() {
+        return dataType;
+    }
+
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(id);
+        dataOutput.writeInt(dataType);
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+        this.id = dataInput.readInt();
+        this.dataType = dataInput.readInt();
+    }
+
+    
 }
