f69a57cbc4328b6489eefb504bbb5b937a4b0600
Snake5932 <shtjnk@yandex.ru>
2021-10-21 20:56:57 +0300

    Scripted auto-commit on change (2021-10-21 20:56:57) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/Reducer.java
+++ src/main/java/ru/bmstu/iu9/lab2/Reducer.java
@@ -4,6 +4,11 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
-public class Reducer extends Reducer<Text, IntWritable, Text, LongWritable {
-    
+import java.io.IOException;
+
+public class Reducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+    }
 }
