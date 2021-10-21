d7f5a70db95ea718e3657bb63f251702212e37c0
Snake5932 <shtjnk@yandex.ru>
2021-10-21 23:55:40 +0300

    Scripted auto-commit on change (2021-10-21 23:55:40) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/App.java
+++ src/main/java/ru/bmstu/iu9/lab2/App.java
@@ -2,7 +2,6 @@ package ru.bmstu.iu9.lab2;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -17,7 +16,8 @@ public class App {
         Job job = Job.getInstance();
         job.setJarByClass(App.class);
         job.setJobName("App");
-        MultipleInputs.addInputPath(job, new Path(args[0], TextInputFormat.class, FlightMapper.class));
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setReducerClass(DelayReducer.class);
         job.setPartitionerClass(CustomPartitioner.class);
