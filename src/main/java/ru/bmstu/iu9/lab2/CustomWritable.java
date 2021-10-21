bb30eee5fbd8332b1fb991f42224b88301b2e377
Snake5932 <shtjnk@yandex.ru>
2021-10-21 22:00:39 +0300

    Scripted auto-commit on change (2021-10-21 22:00:39) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
+++ src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
@@ -1,7 +1,10 @@
 package ru.bmstu.iu9.lab2;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.Text;
 
 public class CustomWritable implements Writable {
-    
+    private Text airportName;
+    private IntWritable delayTime;
 }
