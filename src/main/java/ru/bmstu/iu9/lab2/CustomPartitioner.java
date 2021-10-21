185f7f08e6a65af68f5890c30a818e86b5accf67
Snake5932 <shtjnk@yandex.ru>
2021-10-21 23:20:26 +0300

    Scripted auto-commit on change (2021-10-21 23:20:26) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/CustomPartitioner.java
+++ src/main/java/ru/bmstu/iu9/lab2/CustomPartitioner.java
@@ -1,4 +1,10 @@
 package ru.bmstu.iu9.lab2;
 
-public class CustomPartitioner extends Partitioner<> {
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class CustomPartitioner extends Partitioner<Key, CustomWritable> {
+    @Override
+    public int getPartition(Key key, CustomWritable value, int numPartitions) {
+        
+    }
 }
