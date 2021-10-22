161d530dd39869184139929a17f549619b7498d0
Snake5932 <shtjnk@yandex.ru>
2021-10-22 20:33:15 +0300

    Scripted auto-commit on change (2021-10-22 20:33:15) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/iu9/lab2/FlightMapper.java
@@ -16,7 +16,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, Key, CustomWritable
                 CustomWritable val = new CustomWritable();
                 val.setAirportName(flight[14]);
                 val.setDelayTime((int) Float.parseFloat(flight[17]));
-                System.out.println(k.getDataType());
+                System.out.println(val.getDelayTime());
                 context.write(k, val);
             }
         }
