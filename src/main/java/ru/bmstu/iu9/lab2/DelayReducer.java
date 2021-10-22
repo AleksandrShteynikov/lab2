240552a099ba42ad21888def1874994514fa5b25
Snake5932 <shtjnk@yandex.ru>
2021-10-22 20:41:40 +0300

    Scripted auto-commit on change (2021-10-22 20:41:40) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/DelayReducer.java
+++ src/main/java/ru/bmstu/iu9/lab2/DelayReducer.java
@@ -30,7 +30,7 @@ public class DelayReducer extends Reducer<Key, CustomWritable, Text, Text> {
         if (count != 0) {
             System.out.println(key.getDataType());
             int avg = accum / count;
-            context.write(new Text(String.valueOf(key)), new Text("average: " + avg + ", min: " + min + ", max: " + max));
+            context.write(new Text(key.getId()), new Text("average: " + avg + ", min: " + min + ", max: " + max));
         }
     }
 }
