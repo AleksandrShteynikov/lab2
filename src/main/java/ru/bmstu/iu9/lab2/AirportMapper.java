c653ad965dc78291d1b182237f6bbbd2f1c7ca02
Snake5932 <shtjnk@yandex.ru>
2021-10-22 07:18:58 +0300

    Scripted auto-commit on change (2021-10-22 07:18:58) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
@@ -4,17 +4,21 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.commons.lang3.StringUtils;
+import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Key, CustomWritable> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) {
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() != 0) {
-            String[] airport = value.toString().split(",", 2);
-            airport[0] = StringUtils.strip(airport[0], "\"");
-            airport[1] = StringUtils.strip(airport[0], "\"");
+            final String separator = ",";
+            final String trimmer = "\"";
+            String[] airport = value.toString().split(separator, 2);
+            airport[0] = StringUtils.strip(airport[0], trimmer);
+            airport[1] = StringUtils.strip(airport[0], trimmer);
             CustomWritable val = new CustomWritable();
             val.setAirportName(airport[1]);
-            Key k = new Key()
+            Key k = new Key(Integer.parseInt(airport[0]), 0);
+            context.write(k, val);
         }
     }
 }
