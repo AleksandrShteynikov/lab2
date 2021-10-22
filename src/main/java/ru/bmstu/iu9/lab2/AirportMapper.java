ed32e030cbc4456b63b17873bd57764babb2e397
Snake5932 <shtjnk@yandex.ru>
2021-10-22 07:15:14 +0300

    Scripted auto-commit on change (2021-10-22 07:15:14) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
@@ -10,7 +10,11 @@ public class AirportMapper extends Mapper<LongWritable, Text, Key, CustomWritabl
     protected void map(LongWritable key, Text value, Context context) {
         if (key.get() != 0) {
             String[] airport = value.toString().split(",", 2);
-            
+            airport[0] = StringUtils.strip(airport[0], "\"");
+            airport[1] = StringUtils.strip(airport[0], "\"");
+            CustomWritable val = new CustomWritable();
+            val.setAirportName(airport[1]);
+            Key k = 
         }
     }
 }
--- src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
+++ src/main/java/ru/bmstu/iu9/lab2/CustomWritable.java
@@ -29,6 +29,7 @@ public class CustomWritable implements Writable {
     public String getAirportName() {
         return airportName;
     }
+
     public void setAirportName(String airportName) {
         this.airportName = airportName;
     }
