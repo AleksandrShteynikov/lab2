b1923f33b2a3a14d62530505fad4c0d05d224520
Snake5932 <shtjnk@yandex.ru>
2021-10-22 07:22:47 +0300

    Scripted auto-commit on change (2021-10-22 07:22:47) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/iu9/lab2/FlightMapper.java
@@ -3,10 +3,11 @@ package ru.bmstu.iu9.lab2;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, Key, CustomWritable> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) {
-
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        
     }
 }
