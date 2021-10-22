07f01ba502f8de7d78b63e8a6d49cb2cb83404da
Snake5932 <shtjnk@yandex.ru>
2021-10-22 07:09:38 +0300

    Scripted auto-commit on change (2021-10-22 07:09:38) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
@@ -3,12 +3,13 @@ package ru.bmstu.iu9.lab2;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.commons.lang3.StringUtils;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Key, CustomWritable> {
     @Override
     protected void map(LongWritable key, Text value, Context context) {
         if (key.get() != 0) {
-            String[] airport = stringUt
+            String[] airport = StringUtils.strip()
         }
     }
 }
