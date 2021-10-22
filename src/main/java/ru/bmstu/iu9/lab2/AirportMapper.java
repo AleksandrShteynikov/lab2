319e521f9af0a3014494882c3ede96103a3969c8
Snake5932 <shtjnk@yandex.ru>
2021-10-22 07:00:38 +0300

    Scripted auto-commit on change (2021-10-22 07:00:38) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/iu9/lab2/AirportMapper.java
@@ -1,4 +1,12 @@
 package ru.bmstu.iu9.lab2;
 
-public class AirportMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportMapper extends Mapper<LongWritable, Text, Key, CustomWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) {
+        
+    }
 }
--- src/main/java/ru/bmstu/iu9/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/iu9/lab2/FlightMapper.java
@@ -1,4 +1,12 @@
 package ru.bmstu.iu9.lab2;
 
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class FlightMapper extends Mapper<LongWritable, Text, Key, CustomWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) {
+
+    }
 }
